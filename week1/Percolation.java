c46de7d049699f56397b375ed24947771d126267
marcosfparreiras <marcosfparreiras@gmail.com>
2016-10-08 17:25:09 -0300

    Start implementing Permutation constructor and #open

--- week1/Percolation.java
+++ week1/Percolation.java
@@ -2,14 +2,23 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
   // create n-by-n grid, with all sites blocked
-   public Percolation(int n) {
-     if(n <= 0) {
-       throw new IllegalArgumentException(Integer.toString(n));
-     }
-   }
 
-  //  // open site (row i, column j) if it is not open already
-  //  public void open(int i, int j)
+  private int n;
+
+  public Percolation(int n) {
+    if(n <= 0) {
+      throw new IllegalArgumentException(Integer.toString(n));
+    }
+    this.n = n;
+    WeightedQuickUnionUF unionfind = new WeightedQuickUnionUF(n * n);
+  }
+
+  // open site (row i, column j) if it is not open already
+  public void open(int i, int j) {
+    if(i <= 0 || i > n || j <=0 || j > n) {
+      throw new java.lang.IndexOutOfBoundsException();
+    }
+  }
 
   //  // is site (row i, column j) open?
   //  public boolean isOpen(int i, int j)
--- week1/README.md
+++ week1/README.md
@@ -1,2 +1,12 @@
 # Specification
 Here is the programming assignment [specification](http://coursera.cs.princeton.edu/algs4/assignments/percolation.html) that describes the assignment requirements.
+
+# How to run it?
+1- Compile the files:
+```
+# javac-algs4 Percolation.java
+```
+2- Run it passing the desired value of N as parameter
+```
+# java-algs4 Percolation 10
+```
